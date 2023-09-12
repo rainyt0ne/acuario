a567f8df2dad034974184a33f3852ebe1d51aae5
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-09-12 02:03:18 +0000

    add-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   #ユーザー側
   #URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
