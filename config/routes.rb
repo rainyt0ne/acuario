537ca9bc1695cb74d070ddd7a3bf2d38beb87833
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-09-18 00:04:30 +0000

    last-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,53 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
   #ユーザー側
   #URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
+    registrations: 'public/registrations',
     sessions: "public/sessions"
   }
+
+  #ゲストログイン
+  devise_scope :user do
+    post '/users/guest_session' => 'public/sessions#guest_session'
+    #ユーザー登録失敗時のリダイレクトエラー対策
+    get 'users' => 'public/registrations#new'
+  end
+
+  scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes/about'
+    resources :users, only: [:show, :edit, :update, :destroy] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+      collection do
+        patch 'unsubscribe' => 'users#unscribe'
+        patch 'withdraw' => 'users#withdraw'
+        get 'user_search' => 'users#search'
+      end
+    end
+    resources :posts do
+      resource :likes, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+      collection do
+        get 'search' => 'posts#search', as: 'search'
+      end
+    end
+  end
 
   #管理者側
   #URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root 'homes#top'
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+      resources :comment, only: [:destroy]
+    end
+  end
+
 end
