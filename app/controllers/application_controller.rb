6fd1f2403ea76d27f791b9eb109f359b268747d4
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-09-19 01:39:49 +0000

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  # Deviseコントローラー作動前に実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後、ルートパスに飛ばす
+  def after_sign_in_path_for(source)
+    root_path
+  end
+
+  # 新規登録後、ルートパスに飛ばす
+  def after_sign_up_path_for(source)
+    root_path
+  end
+
+  private
+
+    # カスタムパラメーター（ユーザーネーム）を許可
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(sign_up, keys: [:name])
+    end
+
 end
