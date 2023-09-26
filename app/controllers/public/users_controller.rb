de0f7a631d198d1794c09966e190c14bfaf5eba1
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-09-26 10:21:23 +0000

    fix something

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,18 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
   before_action :ensure_guest, only: [:edit]
-  before_action :ensure_unsubscribe, only: [:show]
   before_action :ensure_withdrawal_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
+    @post = Post.new
     @posts = Post.where(user_id: @user.id).order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def index
+    @user = current_user
+    @post = Post.new
+    @users = User.page(params[:page]).order(created_at: :desc).per(10)
   end
 
   def edit
@@ -23,30 +28,15 @@ class Public::UsersController < ApplicationController
     end
   end
 
-  def unsubscribe
-  end
-
-  # ユーザー検索（ゲストを除く）
-  def search
-    # ゲスト以外を取得し、いなければnilを返す
-    search_users = User.where.not(name: "Guest").search(params[:user_search])
-    if search_users == nil
-      @users = search_users
-    else
-      @users = search_users.page(params[:page]).per(10)
-    end
-  end
-
-  def withdraw
-    @user = current_user
-    # 当該のユーザーを論理削除してセッションをリセットさせる
-    @user.update(is_withdrawal: true)
-    reset_session
-    redirect_to root_path, notice: "退会処理が完了しました。ご利用、ありがとうございました。"
+  # 特定ユーザーのいいねレコードを全て取得
+  def likes
+    @user = User.find(params[:id])
+    likes = Like.where(user_id: @user.id).pluck(:post_id)
+    @like_post = Post.find(likes)
   end
 
   private
-
+    # カスタムパラメーターを許可
     def user_params
       params.require(:user).permit(:name, :email, :introduction, :profile_image)
     end
@@ -68,8 +58,8 @@ class Public::UsersController < ApplicationController
     end
 
     # 退会確認画面でのリロード対策
-    def ensure_unsubscribe
-      if params[:id] == "unsubscribe"
+    def ensure_confirm
+      if params[:id] == "confirm"
         redirect_to root_path, alert: "退会確認画面でリロードされた為、トップページに戻りました。"
       end
     end
