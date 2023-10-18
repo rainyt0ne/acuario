2822475d430ab8df0c0ffd6c8d437865af1c518c
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-10-18 09:33:43 +0000

    last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者ログイン用（email/pass）
+Admin.create!(
+  email: 'admin@gmail.com',
+  password: '123456'
+  )
